e4afe6e3855ecf17d12f00b06425b5766b73ecc7
minchenlee <110967458+minchenlee@users.noreply.github.com>
2023-02-20 21:54:09 +0800

    Revert "create myapp dir and change gitignore"
    This reverts commit 506e2bf7240a73695aaffa5d5ef8cf9d7458f5c6.

--- myapp/routes/healthcheck.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-/* GET healthcheck. */
-router.get('/', function(req, res, next) {
-  res.render('healthcheck', { title: 'OK' });
-});
-
-module.exports = router;
